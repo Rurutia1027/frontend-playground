480d3f60e4daedb54ae859ef8763dd7a807fbdd1
Rurutia1027 <rurutia1027@gmail.com>
2024-12-13 22:47:53 +0800

    add comments for promise

--- javascript-mastery/promises/my-first-promise.js
+++ javascript-mastery/promises/my-first-promise.js
@@ -5,7 +5,16 @@
 // the other one is: reject
 const promise = new Promise((resolve, reject) => {
   setTimeout(() => {
+    // first, only former one of the resolve or reject will be invoked
+    // second, we can handle the parameters by resolve or reject, and the parameters can be more complex than a string
+    // the params we pass to resolve(...) --> will be handled to the then(res: this is the parameter of resolve passed)
+    // and then() function's inner logic is what we called the handler --> how to handle the success response event;
+
+    // the same, the params we pass to reject(...) --> will be handled to the catch(err: this is the parameter of reject funciton passed)
+    // then catch() function's inner logic iw waht we called the handler --> how to handle the fail response event.
     resolve("data back from the server");
+
+    reject({ a: 1, b: 2 });
   }, 3000);
 
   setTimeout(() => {
