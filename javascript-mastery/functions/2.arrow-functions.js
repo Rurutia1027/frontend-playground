729c5af18777bcce3809dd0c5a4b4863a92cb477
Rurutia1027 <rurutia1027@gmail.com>
2024-12-13 14:21:48 +0800

    begin with functions in javascript

---
+++ javascript-mastery/functions/2.arrow-functions.js
@@ -0,0 +1,82 @@
+// Arrow Functions
+
+function getBrand(brand) {
+  console.log(brand);
+  return {
+    brand: brand,
+    website: `www.${brand}.com`,
+  };
+}
+
+// v1: let const variable point to non-arrow function
+const getBrandFuncV1 = function (brand) {
+  console.log("getBrandFuncV1");
+  return {
+    brand: brand,
+    website: `www.${brand}.com`,
+  };
+};
+
+console.log(
+  "invoek getBrandFuncV1: " +
+    JSON.stringify(getBrandFuncV1("TestNonArrowFunc"), null, 1)
+);
+
+// v2: let const variable point to hybird-arrow function
+const getBrandFuncV2 = (brand) => {
+  console.log("getBrandFuncV2");
+  return {
+    brand: brand,
+    website: `www.${brand}.com`,
+  };
+};
+
+console.log(
+  "invoke getBrandFuncV2: " +
+    JSON.stringify(getBrandFuncV2("TestHybirdArrowFunc"), null, 1)
+);
+
+// v3: complete arrow func with single params
+// formular: const func_name = parameters => ({function body codes});
+const getBrandFuncV3 = (brand) => {
+  console.log("getBrandFuncV3");
+  return {
+    brand: brand,
+    website: `www.${brand}.com`,
+  };
+};
+console.log(
+  "invoke getBrandFuncV3: " +
+    JSON.stringify(getBrandFuncV3("TestArrowFuncWithSingleParam"), null, 1)
+);
+
+// v4: complete arrow func with multiple params
+const getBrandFuncV4 = (p1, p2, p3) => {
+  return {
+    parameter1: p1,
+    parameter2: p2,
+    parameter3: p3,
+  };
+};
+
+console.log(
+  "invoke getBrandFuncV4 " +
+    JSON.stringify(getBrandFuncV4(1, 2, "TestArrowWithMultipleParams"), null, 1)
+);
+
+// v5: complete arrow func with multiple params in most simple expression
+// use () to warp the online expression to let the arrow function become the simplest
+const getBrandFuncV5 = (p1, p2, p3) => ({
+  parameter1: p1,
+  parameter2: p2,
+  parameter3: p3,
+});
+
+console.log(
+  "invoke getBrandFuncV5 " +
+    JSON.stringify(
+      getBrandFuncV5(1, 2, "TestArrowWithMultipleParamsMostSimpleExpression"),
+      null,
+      1
+    )
+);
