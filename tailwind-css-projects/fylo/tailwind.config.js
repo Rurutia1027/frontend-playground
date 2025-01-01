e67c3c2a454e2fc6f7d472344b1423cfc08568e3
Rurutia1027 <90094814+Rurutia1027@users.noreply.github.com>
2025-01-01 22:10:30 +0800

    Revert "fix light mode not working issue"

--- tailwind-css-projects/fylo/tailwind.config.js
+++ tailwind-css-projects/fylo/tailwind.config.js
@@ -1,6 +1,6 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./*.html'],
-  darkMode: 'class',
+  content: ["./*.html"],
   theme: {
     extend: {
       colors: {
@@ -14,20 +14,21 @@ module.exports = {
       },
       fontFamily: {
         sans: ['Raleway', 'sans-serif'],
-        opensans: ['Open Sans', 'sans-serif'],
+        opensans: ['Open Sans', 'sans-serif']
       },
       backgroundImage: (theme) => ({
         'logo-dark-mode': "url('../images/logo-dark-mode.svg')",
         'logo-light-mode': "url('../images/logo-light-mode.svg')",
-        'curvy-dark-mode': "url('../images/bg-curvy-dark-mode.svg')",
-        'curvy-light-mode': "url('../images/bg-curvy-light-mode.svg')",
+        "curvy-dark-mode": "url('../images/bg-curvy-dark-mode.svg')",
+        "curylight-mode": "url('../images/bg-curvy-light-mode.svg')",
       }),
     },
   },
   variants: {
     extend: {
       backgroundImage: ['dark'],
-    },
+    }
   },
   plugins: [],
 }
+
