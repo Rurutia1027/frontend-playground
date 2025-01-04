2f2e13cc0be110c360a57814149bd848037cc1f8
Rurutia1027 <rurutia1027@gmail.com>
2025-01-04 16:53:23 +0800

    update codes

---
+++ react-projects/docs/note-1.react.md
@@ -0,0 +1,2 @@
+# React Learning Notes 
+* `index.js` is our JavaScript entry point, we usually need to add some additional code. But normally it's just react and a function. 
--- react-projects/react-app-starter/src/index.js
+++ react-projects/react-app-starter/src/index.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// starting with import react component(s) from react dependency. 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// This is called th ESX modules where esentially we can get a piece of functionality into our file. 
+// This is just the javascript, and this is nothing specific to react now. 
+import React from 'react';  
+import ReactDom from 'react-dom';
+// Here, we are creating our first React component. 
+// A React component alwasy starts as a function. However, to indicate to React that this function 
+// is a **Component**, we must capitalize its name (e.g., MyComponent).
+// The return type of this **Component** function must follow the syntax of JSX(JavaScript XML).
+// which allows us to write HTML-like code within JavaScript. 
+function Greeting() { 
+  return <h4>this is my first component</h4>;  
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// after we finish it nothing will show on the web browser's page. 
+// because index.js is a javascript entry point
+// normally what we need to do in index.js is to setup the component, and importing some libraries. 
+// and those index.js scoped created components will be imported by index.html and then the components
+// can be shown on the website's pages.  
+
+// What's more the components shown to the HTML pages, should be referenced by ID. 
+// And this ID is coming from react's ReactDom this library which locates in the react dependency with the name of 'react-dom'
+
+
+// and here take care of this self-closing tag, don't forget this. 
+ReactDom.render(<Greeting/>, document.getElementById('root')); 
