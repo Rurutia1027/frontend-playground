54514f990ce9c2bcd60cb89bac980d55d19397ee
Rurutia1027 <rurutia1027@gmail.com>
2025-01-05 15:26:47 +0800

    update array manipulation case via useSate

---
+++ react-projects/react-advanced-app/src/tutorial/1-useState/3-useSate-array.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { data } from "../../data";
+
+const UseStateArray = () => { 
+    const [people, setPeople] = React.useState(data); 
+    const handleClick = () => {
+        setPeople([]);
+    }; 
+
+    const removeItem = (id) => {
+        let newPeople = people.filter((person) => {
+            return person.id !== id; 
+        }); 
+        setPeople(newPeople);
+    }; 
+
+    return <>
+        {people.map((person) => { 
+            console.log(person); 
+            const {id, name} = person
+            return <div key={id} className="item">
+                <h4>{name}</h4>
+                <button className="btn" onClick={() => removeItem(id)}>Remove</button>
+            </div>
+        })}
+
+        {people.length > 0 && (<button className="btn" onClick={handleClick}>
+            clear items
+        </button>
+        )}
+        </>; 
+}; 
+
+export default UseStateArray; 
