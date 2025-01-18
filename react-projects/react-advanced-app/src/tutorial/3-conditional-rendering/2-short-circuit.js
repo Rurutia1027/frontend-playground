6cdb5fddd02c5d047ab010c20355d4d77005f431
Rurutia1027 <rurutia1027@gmail.com>
2025-01-18 12:50:31 +0800

    update finished short circut codes

--- react-projects/react-advanced-app/src/tutorial/3-conditional-rendering/2-short-circuit.js
+++ react-projects/react-advanced-app/src/tutorial/3-conditional-rendering/2-short-circuit.js
@@ -2,14 +2,16 @@ import React, { useState } from 'react'
 
 const ShortCircuit = () => {
   const [text, setText] = useState('')
+  const [isError, setIsError] = useState(false)
   const firstValue = text || 'hello world'
   const secondValue = text && 'hello world'
 
   return (
     <>
-      <h1>{text || 'john doe'}</h1>
-      {text && <h1>hello world!</h1>}
-      {!text && <h1>hello world?</h1>}
+      <h1>{text || 'React Short Cicuit'}</h1>
+      <button className='btn' onClick={() => setIsError(!isError)}>Toggle Error</button>
+      {isError && <h1>Error...</h1>}      
+      {isError ? <p>there is an error ...</p>: <p>no error...</p> }
     </>
   )
 }
